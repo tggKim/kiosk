e7b0fc71d3986a26cf82b68691d43e3e5584e595
tgg <qazplmtfgcv123@gmail.com>
2025-01-13 17:59:10 +0900

    CartItem의 equals()메서드 재정의

--- src/main/java/org/example/lv5_challenge/cart/CartItem.java
+++ src/main/java/org/example/lv5_challenge/cart/CartItem.java
@@ -2,6 +2,8 @@ package org.example.lv5_challenge.cart;
 
 import org.example.lv5_challenge.MenuItem;
 
+import java.util.Objects;
+
 public class CartItem {
     private String menuName;
     private double menuPrice;
@@ -29,5 +31,23 @@ public class CartItem {
 
     public int getQuantity() {
         return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-15s|",menuName)+ " W " + menuPrice + " | " + menuDescription;
+    }
+
+    // CartItem이 저장된 List에서 해당 CartItem을 contains()와 indexOf()로 찾을 수 있도록 equals() 재정의함, menuName을 기준으로 구분하도록 했다.
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CartItem cartItem = (CartItem) o;
+        return this.menuName.equals(cartItem.getMenuName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(menuName, menuPrice, menuDescription, quantity);
     }
 }
--- src/main/java/org/example/lv5_challenge/cart/ShoppingCart.java
+++ src/main/java/org/example/lv5_challenge/cart/ShoppingCart.java
@@ -1,4 +1,9 @@
 package org.example.lv5_challenge.cart;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
+    private List<CartItem> cartItems = new ArrayList<>();
+
 }
