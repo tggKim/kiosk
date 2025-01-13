c1b2580a3486c1b4786a00a903fd1838040c9c93
tgg <qazplmtfgcv123@gmail.com>
2025-01-13 11:00:17 +0900

    scanner을 통한 입력과 정규식으로 판별하는 기능ì추가

--- src/main/java/org/example/lv1/Main.java
+++ src/main/java/org/example/lv1/Main.java
@@ -1,7 +1,39 @@
 package org.example.lv1;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Main {
+
+    private static final String NUMBER_REG = "^[0-9]+$";
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+
+
+    }
+
+    private static String readFunctionFlag(){
+        String functionFlag = scanner.nextLine();
+        while(!Pattern.matches(NUMBER_REG, functionFlag) || Integer.parseInt(functionFlag) > 4){
+            if(!Pattern.matches(NUMBER_REG, functionFlag)){
+                System.out.print("잘못된 입력 형식입니다. 1-4 사이의 정수를 입력해주세요: ");
+            }
+            else{
+                System.out.print("잘못된 번호입니다. 1-4 사이의 번호를 입력해주세요: ");
+            }
+            functionFlag = scanner.nextLine();
+        }
+        return functionFlag;
+    }
+
+    private static String readNumber(){
+        String number = scanner.nextLine();
+        while(!Pattern.matches(NUMBER_REG,number)){
+            System.out.print("정수 형태를 입력해 주세요: ");
+            number = scanner.nextLine();
+        }
+        return number;
     }
 }
