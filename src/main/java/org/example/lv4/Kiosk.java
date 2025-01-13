62786ace9055f9e8717f437abde9e9f2659ed885
tgg <qazplmtfgcv123@gmail.com>
2025-01-13 15:28:06 +0900

    Kioskí

--- src/main/java/org/example/lv4/Kiosk.java
+++ src/main/java/org/example/lv4/Kiosk.java
@@ -10,6 +10,7 @@ public class Kiosk {
     private final String NUMBER_REG = "^[0-9]+$";
     private Scanner scanner = new Scanner(System.in);
     private List<Menu> menuCategory;
+    private Menu menu;
 
     public Kiosk(List<Menu> menuCategory){
         this.menuCategory = menuCategory;
@@ -22,10 +23,8 @@ public class Kiosk {
         // 반복해서 키오스크 메뉴 출력하는 반복문
         while(!functionFlag.equals("0")){
 
-            // 메인 카테고리 출력
+            // 카테고리 출력
             System.out.println("[ MAIN MENU ]");
-
-            // 메인 카테고리 출력
             for(int i = 0; i < menuCategory.size(); i++){
                 System.out.println((i + 1) + ". " + menuCategory.get(i).getCategory());
             }
@@ -33,25 +32,34 @@ public class Kiosk {
 
             // 사용자에게 입력을 받음
             functionFlag = readFunctionFlag();
-            int menuNumber = Integer.parseInt(functionFlag);
+            int categoryNumber = Integer.parseInt(functionFlag);
             // 입력한 숫자가 0이 아니라면 해당되는 메뉴를 출력, 0이면 종료
-            if(menuNumber == 0){
+            if(categoryNumber == 0){
                 System.out.println("============================================================================");
                 System.out.println("프로그램을 종료합니다.");
                 System.out.println("============================================================================");
                 break;
             }
 
+            // 카테고리에 해당되는 Menu 객체를 불러옴
+            menu = menuCategory.get(categoryNumber - 1);
 
-//            // 카테고리별 메뉴 출력
-//            System.out.println("[ MAIN MENU ]");
-//
-//            // 카테고리별 메뉴 출력
-//            for(int i = 0; i < menus.size(); i++){
-//                System.out.println((i + 1) + ". " + menus.get(i).getCategory());
-//            }
-//            System.out.println("exit");
-//            //System.out.println(String.format("0. %-15s| 종료","exit"));
+            // 카테고리별 메뉴 출력
+            System.out.println("\n[ " + menu.getCategory() + " MENU ]");
+            menu.showMenu();
+            System.out.println("0. 뒤로가기");
+
+            // 카테고리별 메뉴 선택
+            int menuNumber = readMenuNumber();
+            // 0을 입력하면 continue해서 다시 카테고리 선택 메뉴 보여줌
+            if(menuNumber == 0){
+                System.out.println();
+                continue;
+            }
+
+            System.out.println("============================================================================");
+            System.out.println(menu.getMenus().get(menuNumber - 1));
+            System.out.println("============================================================================");
 
             // 한 칸 띄기
             System.out.println();
@@ -67,5 +75,15 @@ public class Kiosk {
             functionFlag = scanner.nextLine();
         }
         return functionFlag;
+    }
+
+    private int readMenuNumber(){
+        String menuNumberString = scanner.nextLine();
+        // 정규식의 표현과 맞지 않거나 메뉴에 없는 숫자를 입력하면 반복해서 입력하도록 지시함
+        while(!Pattern.matches(NUMBER_REG, menuNumberString) || Integer.parseInt(menuNumberString) > menu.getMenus().size()){
+            System.out.print("잘못된 입력 형식입니다. 0-" + (menu.getMenus().size()) + " 사이의 번호를 입력해주세요: ");
+            menuNumberString = scanner.nextLine();
+        }
+        return Integer.parseInt(menuNumberString);
     }
 }
