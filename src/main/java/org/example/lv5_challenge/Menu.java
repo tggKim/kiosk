27bf353cd79616a0beb3511f2dae34bae0ea963a
tgg <qazplmtfgcv123@gmail.com>
2025-01-14 13:32:13 +0900

    showMenu() 메서드 람다와 스트림 사용하도록 수정

--- src/main/java/org/example/lv5_challenge/Menu.java
+++ src/main/java/org/example/lv5_challenge/Menu.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Menu {
     private List<MenuItem> menus;
     private String category;
+    int sequence = 0;
 
     public Menu(String category, List<MenuItem> menus){
         this.category = category;
@@ -13,9 +14,11 @@ public class Menu {
 
     // 메뉴를 출력하는 메서드
     public void showMenu(){
-        for(int i = 0; i < menus.size(); i++){
-            System.out.println((i + 1) + ". " + menus.get(i));
-        }
+        menus.stream().forEach(a -> System.out.println((sequence + 1) + ". " + menus.get(sequence++)));
+        sequence = 0;
+//        for(int i = 0; i < menus.size(); i++){
+//            System.out.println((i + 1) + ". " + menus.get(i));
+//        }
     }
 
     public List<MenuItem> getMenus() {
